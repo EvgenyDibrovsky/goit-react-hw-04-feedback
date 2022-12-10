26449de00be83ca04daf9ffd1f687c4ab3e4ac0d
Evgeny Dibrovsky <105384282+EvgenyDibrovsky@users.noreply.github.com>
2022-12-10 15:48:57 +0100

    HW-4

---
+++ src/components/Statistics/Statistics.jsx
@@ -0,0 +1,36 @@
+import propTypes from 'prop-types';
+import css from './Statistics.module.css';
+
+export const Statistics = ({
+  good,
+  neutral,
+  bad,
+  total,
+  positivePercentage,
+}) => (
+  <ul className={css.feedbackList}>
+    <li className={css.feedbackListItem}>😀 Good: {good}</li>
+    <li className={css.feedbackListItem}>😐 Neutral: {neutral}</li>
+    <li className={css.feedbackListItem}>🙁 Bad: {bad}</li>
+    <li className={css.feedbackTotal}>🏁 Total: {total}</li>
+    <li className={css.feedbackPositive}>
+      Positive feedback: {positivePercentage} %
+    </li>
+  </ul>
+);
+
+Statistics.propTypes = {
+  good: propTypes.number.isRequired,
+  neutral: propTypes.number.isRequired,
+  bad: propTypes.number.isRequired,
+  total: propTypes.number.isRequired,
+  positivePercentage: propTypes.number.isRequired,
+};
+
+Statistics.propTypes = {
+  good: propTypes.number.isRequired,
+  neutral: propTypes.number.isRequired,
+  bad: propTypes.number.isRequired,
+  total: propTypes.number.isRequired,
+  positivePercentage: propTypes.number.isRequired,
+};
